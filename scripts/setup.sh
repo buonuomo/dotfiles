4300af66a8c58b2d0e7e0ca54b4aad95e8ed5520
Noah Goodman <ngoodman@uchicago.edu>
2018-09-03 13:06:53 -0400

    Working prototype of setup script with support for zsh and bash modes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-for i in $(cat files.txt); do
-    if [ ! -f ../$i ] && [ ! -d ../$i ]; then
-        ln -s dotfiles/$i ../$i
+# Directory where this script is located
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# Function to read links from file and make them
+mkLinks () {
+    for i in $(cat $DIR/$1)
+    do
+        lnArgs=(${i//,/ })
+        ln -sri $DIR/../configs/${lnArgs[0]} "${lnArgs[1]/#\~/$HOME}"
+    done
+}
+
+# Main
+read -p "Do you currently have zsh or would like to install it (y/n)? " doZsh
+
+if [ $doZsh = "y" ] || [ $doZsh = "Y" ]
+then
+    if [[ "$(command -v zsh)" ]]
+    then
+        :
+    else
+        echo Installing zsh...
+        sudo apt-get install zsh
     fi
-done
-
+    mkLinks zsh.txt
+else
+    mkLinks bash.txt
+fi
