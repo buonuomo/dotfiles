4e775923d65af85185f7d3f7fc3e4c530b4e21d7
Noah Goodman <ngoodman@uchicago.edu>
2019-08-09 14:12:49 -0500

    some elm and cpp config mostly

--- configs/alii.zsh
+++ configs/alii.zsh
@@ -1,5 +1,6 @@
-# List of personal alii (aliuses?) for my convenience
+# List of personal alii (aliases?) for my convenience
 
+alias elm-repl='elm repl | sed -r "{s/\x1b\[90m \:/\x1b\[89m \:\x1b\[96m/;ty; N; s/\x1b\[90m(\n.*)\:/\x1b\[89m\1\:\x1b\[96m/;P;D;:y}"'
 alias emacs=vim
 alias nano=vim
 alias :q=exit
